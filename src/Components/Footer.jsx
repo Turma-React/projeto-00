1c9dbdc66cf8a96f4a4754c8f658aa32e55081e0
Mauro IFMG <mauro.henrique@ifmg.edu.br>
2025-12-24 12:12:45 +0000

    fix: contato incluso no footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer>Todos os direitos reservados</footer>
+      <footer>Todos os direitos reservados contato@empresa.com</footer>
     </>
   );
 };
